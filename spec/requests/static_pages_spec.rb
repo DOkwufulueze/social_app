e83de0608f2913c473fcf3f92ac1f663dc92a2a5
Daniel Okwufulueze <daniel@dealdey.com>
2015-10-18 22:59:59 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  # describe "Home pages" do
+  #   it "should have the content 'Sample App'" do
+  #     get '/static_pages/home'
+  #     expect(response).to be_success
+  #     # expect(response).to have_selector("h1", content: 'Sample App')
+  #   end
+  # end
+
+  describe "Home page" do
+    it "exists" do
+      get '/static_pages/home'
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "About page" do
+    it "exists" do
+      get '/static_pages/about'
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
